7619cc2846b29e02d81ea6c674b71eb148e8e373
Hunain Ali <119546006+HunainBaloch@users.noreply.github.com>
2025-01-08 02:50:56 +0500

    Initial commit

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+dict1 = {"good": "Something pleasant", "fetch": "to bring", "1": "the number 1"}
+
+dict2 = {"Hunain": 99, "Hasnain": 88, "Zain": 90, "Sahil": 78}
+
+print(dict1["good"])
+
+print(dict2["Hunain"])
--- list_methods.py
+++ list_methods.py
@@ -2,3 +2,7 @@ l1 = [3, 5, 245, 299, 99, 21, "Hunain"]
 
 print(type(l1))
 print(l1)
+
+l1.remove("Hunain")
+l1.sort()
+print(l1)
